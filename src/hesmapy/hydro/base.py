db06827f5b6a57a742e06f02509d99d44e207d8e
Alexander Holas <alex.holas@gmx.de>
2023-11-09 23:04:22 -0500

    first complete schema

--- src/hesmapy/hydro/base.py
+++ src/hesmapy/hydro/base.py
@@ -10,15 +10,48 @@ class Hydro1D:
                 "name": {"type": "string"},
                 "schema": {"type": "string"},
                 "sources": {
+                    "type": "array",
+                    "items": {
+                        "type": "object",
+                        "properties": {
+                            "bibcode": {"type": "string"},
+                            "reference": {"type": "string"},
+                            "url": {"type": "string"},
+                        },
+                    },
+                },
+                "units": {
                     "type": "object",
                     "properties": {
-                        "bibcode": {"type": "string"},
-                        "reference": {"type": "string"},
-                        "url": {"type": "string"},
+                        "radius": {"type": "string"},
+                        "density": {"type": "string"},
+                        "pressure": {"type": "string"},
+                        "temperature": {"type": "string"},
+                        "mass": {"type": "string"},
+                        "velocity": {"type": "string"},
+                        "time": {"type": "string"},
+                    },
+                },
+                "data": {
+                    "type": "array",
+                    "minItems": 2,
+                    "items": {
+                        "type": "object",
+                        "properties": {
+                            "radius": {"type": "number"},
+                            "density": {"type": "number"},
+                            "pressure": {"type": "number"},
+                            "temperature": {"type": "number"},
+                            "mass": {"type": "number"},
+                            "velocity": {"type": "number"},
+                            "time": {"type": "number"},
+                            "\bx[A-Z][a-z][0-9]{0,3}\b": {"type": "number"},
+                        },
+                        "required": ["radius", "density"],
                     },
                 },
             },
-            "required": ["name"],
+            "required": ["name", "data"],
         }
 
         self.path = path
