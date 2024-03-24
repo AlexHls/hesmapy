671acc76b79ebad2b9b1975aa97d51b4dd04bbb9
Alexander Holas <alex.holas@gmx.de>
2024-03-24 16:36:55 +0100

    Add loader function

--- src/hesmapy/base.py
+++ src/hesmapy/base.py
@@ -1,4 +1,5 @@
 from hesmapy.hydro.hydro1d import Hydro1D
+from hesmapy.lightcurves.rt_lightcurve import RTLightcurve
 
 
 def load_hydro_1d(path):
@@ -16,3 +17,20 @@ def load_hydro_1d(path):
 
     """
     return Hydro1D(path)
+
+
+def load_rt_lightcurve(path):
+    """Load a radiative transfer lightcurve from a JSON file.
+
+    Parameters
+    ----------
+    path : str
+        Path to the JSON file.
+
+    Returns
+    -------
+    rt : RTLightcurve
+        RTLightcurve object.
+
+    """
+    return RTLightcurve(path)
