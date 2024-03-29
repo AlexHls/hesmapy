5fe99502a50cde168385454b39dd27e671fe657f
Alexander Holas <alex.holas@gmx.de>
2024-03-29 10:18:57 +0100

    loader test

--- tests/test_loader.py
+++ tests/test_loader.py
@@ -2,7 +2,7 @@ import unittest
 import os
 import json
 from tempfile import NamedTemporaryFile
-from hesmapy.base import load_hydro_1d, Hydro1D
+from hesmapy.base import load_hydro_1d, Hydro1D, load_rt_lightcurve, RTLightcurve
 
 
 class TestLoadHydro1D(unittest.TestCase):
@@ -57,6 +57,48 @@ class TestLoadHydro1D(unittest.TestCase):
         hydro = load_hydro_1d(path)
         os.unlink(path)
         self.assertIsInstance(hydro, Hydro1D)
+
+
+class TestLoadRTLightcurve(unittest.TestCase):
+    def setUp(self):
+        self.valid_data = {
+            "model": {
+                "name": "test",
+                "schema": "test_schema",
+                "sources": [
+                    {
+                        "bibcode": "test",
+                        "reference": "test",
+                        "url": "test",
+                    }
+                ],
+                "units": {
+                    "time": "test",
+                    "band1": "test",
+                    "band2": "test",
+                },
+                "data": [
+                    {
+                        "time": 1,
+                        "band1": 1,
+                        "band2": 1,
+                    },
+                    {
+                        "time": 2,
+                        "band1": 2,
+                        "band2": 2,
+                    },
+                ],
+            },
+        }
+
+    def test_load_rt_lightcurve(self):
+        with NamedTemporaryFile(mode="w", delete=False) as f:
+            json.dump(self.valid_data, f)
+            path = f.name
+        rt = load_rt_lightcurve(path)
+        os.unlink(path)
+        self.assertIsInstance(rt, RTLightcurve)
 
 
 if __name__ == "__main__":
